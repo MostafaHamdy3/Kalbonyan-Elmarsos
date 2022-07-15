3329d9f4bee24eba342e6116b48f477f0353ee99
MostafaHamdy3 <73959716+MostafaHamdy3@users.noreply.github.com>
2022-07-15 21:37:52 +0200

    Add files via upload

---
+++ 02-Udemy/-02-Js-Jonas/Challenges/JavaScript Fundamentals – Part 2/01-Challenges/challenge1.js
@@ -0,0 +1,24 @@
+///////////////////
+// Challenge (1) //
+///////////////////
+/*
+const average = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+function checkWinner(avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas)
+    console.log(`Dolphins win 🏆 (${avgDolphins} vs. ${avgKoalas})`);
+  else if (avgKoalas >= 2 * avgDolphins)
+    console.log(`Koalas win 🏆 (${avgKoalas} vs. ${avgDolphins})`);
+  else console.log("No team wins!");
+}
+
+let avgDolphins = average(44, 23, 71);
+let avgKoalas = average(65, 54, 49);
+console.log(avgDolphins, avgKoalas);
+checkWinner(avgDolphins, avgKoalas);
+
+avgDolphins = average(85, 54, 41);
+avgKoalas = average(23, 34, 27);
+console.log(avgDolphins, avgKoalas);
+checkWinner(avgDolphins, avgKoalas);
+*/
