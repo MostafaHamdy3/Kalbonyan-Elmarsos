a30311c619e0fd71f0de1d35ea76de72751fa021
MostafaHamdy3 <73959716+MostafaHamdy3@users.noreply.github.com>
2022-07-15 18:09:28 +0200

    Add files via upload

---
+++ 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/FavoriteCities.py
@@ -0,0 +1,7 @@
+# Prints out the name of a favorite city
+def favorite_city(name):
+   print("One of my favorite cities is", name)
+
+favorite_city("Cairo, Egypt")
+favorite_city("Santa Barbara, California")
+favorite_city("London, England")
---
+++ 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Guessing.py
@@ -0,0 +1,8 @@
+guess = input("What's my favorite food? ")
+
+if guess == "meat":
+    print("Yep! So amazing!")
+else:
+    print("Yuck! That's not it!")
+
+print("Thanks for playing!")
