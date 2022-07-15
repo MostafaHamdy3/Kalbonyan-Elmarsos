74f70745fe654d4156abb279b8a09216bee49822
MostafaHamdy3 <73959716+MostafaHamdy3@users.noreply.github.com>
2022-07-15 21:38:40 +0200

    Add files via upload

---
+++ 02-Udemy/-02-Js-Jonas/Challenges/JavaScript Fundamentals – Part 2/03-Challenges/challenge3.js
@@ -0,0 +1,36 @@
+///////////////////
+// Challenge (3) //
+///////////////////
+/*
+const mark = {
+   fullName: "Mark Miller",
+   mass: 78,
+   height: 1.69,
+ 
+   calcBMI: function () {
+     this.bmi = this.mass / this.height ** 2;
+     return this.bmi;
+   },
+ };
+ const john = {
+   fullName: "John Smith",
+   mass: 92,
+   height: 1.95,
+ 
+   calcBMI: function () {
+     this.bmi = this.mass / this.height ** 2;
+     return this.bmi;
+   },
+ };
+ mark.calcBMI() > john.calcBMI()
+   ? console.log(
+       `${mark.fullName}'s BMI (${mark.calcBMI()}) is heigher than ${
+         john.fullName
+       }'s (${john.calcBMI()})`
+     )
+   : console.log(
+       `${john.fullName}'s BMI (${john.calcBMI()}) is heigher than ${
+         mark.firstName
+       }'s (${mark.calcBMI()})`
+     );
+ */
