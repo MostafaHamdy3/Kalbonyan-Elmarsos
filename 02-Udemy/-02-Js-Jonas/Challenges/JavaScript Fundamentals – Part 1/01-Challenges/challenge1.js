4b14a7e85d821ba0b03220700dc504a7a4916b3c
MostafaHamdy3 <73959716+MostafaHamdy3@users.noreply.github.com>
2022-07-15 21:31:23 +0200

    Add files via upload

---
+++ 02-Udemy/-02-Js-Jonas/Challenges/JavaScript Fundamentals – Part 1/01-Challenges/challenge1.js
@@ -0,0 +1,34 @@
+/////////////////
+// CHALLENGE 1 //
+/////////////////
+/*
+// TEST DATA 1
+let weightsMark = 78;
+let heightMark = 1.69;
+let weightJohn = 92;
+let heightJohn = 1.95;
+
+let BMI_mark = weightsMark / heightMark ** 2;
+let BMI_john = weightJohn / heightJohn ** 2;
+
+console.log(BMI_mark, BMI_john);
+
+let markHeightBMI = BMI_mark > BMI_john;
+
+console.log(markHeightBMI);
+
+// TEST DATA 2
+weightsMark = 95;
+heightMark = 1.88;
+weightJohn = 85;
+heightJohn = 1.76;
+
+BMI_mark = weightsMark / heightMark ** 2;
+BMI_john = weightJohn / heightJohn ** 2;
+
+console.log(BMI_mark, BMI_john);
+
+markHeightBMI = BMI_mark > BMI_john;
+
+console.log(markHeightBMI);
+*/
---
+++ 02-Udemy/-02-Js-Jonas/Challenges/JavaScript Fundamentals – Part 1/01-Challenges/challenge2.js
@@ -0,0 +1,44 @@
+/////////////////
+// CHALLENGE 2 //
+/////////////////
+/*
+// TEST DATA 1
+let weightsMark = 78;
+let heightMark = 1.69;
+let weightJohn = 92;
+let heightJohn = 1.95;
+
+let BMI_mark = weightsMark / heightMark ** 2;
+let BMI_john = weightJohn / heightJohn ** 2;
+
+let markHeightBMI = BMI_mark > BMI_john;
+
+if (markHeightBMI)
+  console.log(
+    "Mark's BMI (" + BMI_mark + ") is higher than John's (" + BMI_john + ")!"
+  );
+else
+  console.log(
+    "John's BMI (" + BMI_john + ") is higher than Mark's (" + BMI_mark + ")!"
+  );
+
+// TEST DATA 2
+weightsMark = 95;
+heightMark = 1.88;
+weightJohn = 85;
+heightJohn = 1.76;
+
+BMI_mark = weightsMark / heightMark ** 2;
+BMI_john = weightJohn / heightJohn ** 2;
+
+markHeightBMI = BMI_mark > BMI_john;
+
+if (markHeightBMI)
+  console.log(
+    "Mark's BMI " + BMI_mark + " is higher than John's " + BMI_john + "!"
+  );
+else
+  console.log(
+    "John's BMI (" + BMI_john + ") is higher than Mark's (" + BMI_mark + ")!"
+  );
+*/
