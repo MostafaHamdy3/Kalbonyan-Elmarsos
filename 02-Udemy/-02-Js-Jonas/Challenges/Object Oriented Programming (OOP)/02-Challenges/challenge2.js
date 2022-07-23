33362319bceae09095fb1be74e274a04fd9be7e2
Mostafa Hamdy <73959716+MostafaHamdy3@users.noreply.github.com>
2022-07-23 15:44:45 +0200

    Add files via upload

---
+++ 02-Udemy/-02-Js-Jonas/Challenges/Object Oriented Programming (OOP)/02-Challenges/challenge2.js
@@ -0,0 +1,29 @@
+"use strict";
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed}km/h`);
+  }
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed}km/h`);
+  }
+  get speedUS() {
+    return `${this.speed / 1.6}mi/h`;
+  }
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const ford = new CarCl("Ford", 120);
+console.log(ford.speedUS);
+ford.accelerate();
+ford.brake();
+ford.speedUS = 100;
+console.log(ford);
