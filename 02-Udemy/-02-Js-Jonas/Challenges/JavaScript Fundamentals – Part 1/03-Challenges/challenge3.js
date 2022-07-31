f48a5d1c656bb79e0930e5b5fa903403555111c4
Mostafa Hamdy <73959716+MostafaHamdy3@users.noreply.github.com>
2022-07-31 18:22:13 +0200

    Add files via upload

---
+++ 02-Udemy/-02-Js-Jonas/Challenges/JavaScript Fundamentals – Part 1/03-Challenges/challenge3.js
@@ -0,0 +1,36 @@
+/////////////////
+// CHALLENGE 3 //
+/////////////////
+/*
+let averageDolphins = (96 + 108 + 89) / 3;
+let averageKoalas = (88 + 91 + 110) / 3;
+console.log(averageDolphins, averageKoalas);
+
+if (averageDolphins > averageKoalas) console.log("Dolphins win the trophy 🏆");
+else if (averageDolphins < averageKoalas)
+  console.log("Koalas win the trophy 🏆");
+else console.log("equals");
+
+averageDolphins = (97 + 112 + 101) / 3;
+averageKoalas = (109 + 95 + 123) / 3;
+console.log(averageDolphins, averageKoalas);
+if (averageDolphins > averageKoalas && averageDolphins > 99)
+  console.log("Dolphins win the trophy 🏆");
+else if (averageDolphins < averageKoalas && averageKoalas > 99)
+  console.log("Koalas win the trophy 🏆");
+else console.log("NO one of teams win!");
+
+averageKoalas = (109 + 95 + 106) / 3;
+console.log(averageDolphins, averageKoalas);
+if (averageDolphins > averageKoalas && averageDolphins > 99)
+  console.log("Dolphins win the trophy 🏆");
+else if (averageDolphins < averageKoalas && averageKoalas > 99)
+  console.log("Koalas win the trophy 🏆");
+else if (
+  averageDolphins === averageKoalas &&
+  averageDolphins > 99 &&
+  averageKoalas > 99
+)
+  console.log("Both win the trophy!");
+else console.log("NO one of the teams win!");
+*/
